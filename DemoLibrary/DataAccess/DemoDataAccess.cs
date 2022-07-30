023fc048bf9c3804251667a2dc4e64f8e4014c7a
Kristijan Prematarov <Kristijan.Prematarov@opengi.mk>
2022-07-30 20:41:01 +0200

    Add project files.

---
+++ DemoLibrary/DataAccess/DemoDataAccess.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using DemoLibrary.DataAccess.Models;
+
+namespace DemoLibrary.DataAccess
+{
+    public class DemoDataAccess
+    {
+        private List<PersonModel> people = new();
+
+        public DemoDataAccess()
+        {
+            people.Add(new PersonModel() { Id = 1, FirstName = "Kristijan", LastName = "Prematarov" });
+            people.Add(new PersonModel() { Id = 2, FirstName = "Ivana", LastName = "Kocevska" });
+        }
+
+        public List<PersonModel> GetPeople()
+        {
+            return people;
+        }
+
+        public PersonModel InsertPerson(string firstName, string lastName)
+        {
+            PersonModel p = new()
+            {
+                Id = people.Max(x => x.Id) + 1,
+                FirstName = firstName,
+                LastName = lastName
+            };
+
+            people.Add(p);
+
+            return p;
+        }
+    }
+}
